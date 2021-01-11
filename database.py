d7970e67065d1ec2d335523a4f4846db597c1f29
gustavoavrj <gustavo.avrj@gmail.com>
2021-01-10 23:38:24 -0600

    add heroku

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:123@localhost:3306/restapi"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://wooi58em3zup4aov:mdh45eaf37nttql3@u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ififatv96npyl02f"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
